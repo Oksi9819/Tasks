c537e2d953b88929deecab3df5e4487cfe0dc9c3
OksiFoxy <oksanaostapuk@gmail.com>
2022-06-05 16:27:35 +0300

    Update Task1.php
    Task1

--- Task1/Task1.php
+++ Task1/Task1.php
@@ -8,36 +8,13 @@ use Exception;
 
 class Task1
 {
-private $number;
-
-    public function __construct($number)
+    public function main($number):string
     {
-        $this->setNumber($number);
-    }
-
-    public function getNumber():int
-    {
-        return $this->number;
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function setNumber($number):void
-    {
-        if (is_int($number))
-        {
-            $this->number = $number;
-        }else throw new Exception('Неподходящий тип. Необходимо число.');
-    }
-
-    public function main():string
-    {
-        return $this->number>30
+        return $number>30
             ? "More than 30"
-            : ($this->number>20
+            : ($number>20
             ? "More than 20"
-            : ($this->number>10
+            : ($number>10
             ? "More than 10"
             : "Less than 10"));
     }
