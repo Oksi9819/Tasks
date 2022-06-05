b7008a83a7763f5a248cbb03dacc4bee0fdf5079
OksiFoxy <oksanaostapuk@gmail.com>
2022-06-06 00:24:53 +0300

    Edit methode

--- Task4/Task4.php
+++ Task4/Task4.php
@@ -7,56 +7,18 @@ use Exception;
 
 class Task4
 {
-    private $stringToEdit;
-
     /**
      * @throws Exception
      */
-    public function __construct(string $str)
-    {
-        $this->setStringToEdit($str);
-    }
-
-    /**
-     * @return string
-     */
-    public function getStringToEdit(): string
-    {
-        return $this->stringToEdit;
-    }
-
-    /**
-     * @param mixed $str
-     * @throws Exception
-     */
-    public function setStringToEdit($str): void
+    public function main(string $str): string
     {
         $str = trim($str);
-        if (strpos($str, '_') !== false || strpos($str, '-') !== false || strpos($str, ' ') !== false) {
-            $this->stringToEdit = $str;
+        $del = ['_', '-', ' '];
+        if (strpos($str, $del[0]) !== false || strpos($str, $del[1]) !== false || strpos($str, $del[2]) !== false)
+        {
+            $del = ['_', '-', ' '];
+            $str = ucwords(str_replace($del, ' ', $str), ' ');
+            return str_replace(' ', '', $str);
         } else throw new Exception('Строка должна содержать один из символов "_", "-", " "');
-    }
-
-    public function main(): string
-    {
-        $str = $this->stringToEdit;
-        $a=explode('_', $str);
-        for ($i=0; $i<count($a); $i++)
-        {
-            $a[$i]=ucfirst($a[$i]);
-        }
-        $str=implode($a);
-        $a=explode('-', $str);
-        for ($i=0; $i<count($a); $i++)
-        {
-            $a[$i]=ucfirst($a[$i]);
-        }
-        $str=implode($a);
-        $a=explode(' ', $str);
-        for ($i=0; $i<count($a); $i++)
-        {
-            $a[$i]=ucfirst($a[$i]);
-        }
-        return implode($a);
     }
 }
