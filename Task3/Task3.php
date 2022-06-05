d41d2ff9ddf8c3ea0c73d0c53ca0f38ab8cbdf05
OksiFoxy <oksanaostapuk@gmail.com>
2022-06-06 00:13:36 +0300

    Edit methode

--- Task3/Task3.php
+++ Task3/Task3.php
@@ -6,35 +6,15 @@ namespace src;
 
 class Task3
 {
-    private $number;
-
-    public function __construct(int $number)
+    public function main(int $number):int
     {
-        $this->setNumber($number);
-    }
-
-    public function setNumber(int $number): void
-    {
-        $this->number = $number;
-    }
-
-    /**
-     * @return int
-     */
-    public function getNumber(): int
-    {
-        return $this->number;
-    }
-
-    public function main():int
-    {
-        if ($this->number <> 0)
+        if ($number <> 0)
         {
-            if ($this->number < 0)
+            if ($number < 0)
             {
-                $this->number*=(-1);
+                $number*=(-1);
             }
-            return ($this->number - 1) % 9 + 1;
+            return ($number - 1) % 9 + 1;
         }else return 0;
     }
 }
