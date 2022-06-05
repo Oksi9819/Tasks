821b220a7118cd9c04ef3da94f364bdc6abe2853
OksiFoxy <oksanaostapuk@gmail.com>
2022-06-06 00:43:47 +0300

    Delete other tasks

--- Task8/Task8.php
+++ Task8/Task8.php
@@ -3,37 +3,14 @@
 
 namespace src;
 
-
-use Exception;
-
 class Task8
 {
-    private $json;
-
-    /**
-     * @throws Exception
-     */
-    public function __construct(string $json)
+    public function main(string $json):void
     {
-        $this->setJson($json);
-    }
-
-    /**
-     * @param mixed $json
-     */
-    public function setJson(string $json): void
-    {
-       $this->json = $json;
-    }
-
-    public function main():void
-    {
-        $result = json_decode($this->json, true);
+        $result = json_decode($json, true);
         array_walk_recursive($result,function ($value, $key)
         {
             echo "$key : $value"."\n";
         });
     }
 }
- $json=new Task8('{"Title": "The Cuckoos Calling", "Author": "Robert Galbraith", "Detail": {"Publisher": "Little Brown"}}');
- $json->main();
