5ef0bbfebdea0b29aaacd669c36848f78b64b598
OksiFoxy <oksanaostapuk@gmail.com>
2022-06-06 00:18:26 +0300

    Edit methode

--- Task2/Task2.php
+++ Task2/Task2.php
@@ -9,21 +9,10 @@ use Exception;
 
 class Task2
 {
-    private $date;
-
     /**
      * @throws Exception
      */
-    public function __construct(string $date)
-    {
-        $this->setDate($date);
-    }
-
-
-    /**
-     * @throws Exception
-     */
-    public function setDate(string $date): void
+    public function main($date):int
     {
         if(!DateTime::createFromFormat('d.m.Y', $date))
         {
@@ -33,41 +22,23 @@ class Task2
         {
             throw new Exception('Необходимо, чтобы дата соответствовала григорианскому календарю.');
         }else  $today=new DateTime( "now", new DateTimeZone('Europe/Minsk'));
+
         if ($today->format('Y') > $v->format('Y'))
         {
-            $this->date=DateTime::createFromFormat('d.m.Y', $date);
+            $birthday=$date;
         }elseif ($today->format('Y') == $v->format('Y'))
         {
             if ($today->format('n') > $v->format('n'))
             {
-                $this->date=DateTime::createFromFormat('d.m.Y', $date);
+                $birthday=$date;
             }elseif ($today->format('n') == $v->format('n'))
             {
                 if ($today->format('j') > $v->format('j'))
                 {
-                    $this->date=DateTime::createFromFormat('d.m.Y', $date);
+                    $birthday=$date;
                 }else throw new Exception('Дата рождения должна быть меньше текущей даты.');
             }else throw new Exception('Дата рождения должна быть меньше текущей даты.');
         }else throw new Exception('Дата рождения должна быть меньше текущей даты.');
-    }
-
-    /**
-     * @return DateTime|false
-     */
-    public function getDate():DateTime
-    {
-        return $this->date;
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function main():int
-    {
-        $today=new DateTime( "now", new DateTimeZone('Europe/Minsk'));
-        $birthday=$this->date;
-        echo $birthday->format('d.m.Y')."       ";
-        echo $today->format('d.m.Y')."       ";
         $birthday->setDate($today->format('Y'), $birthday->format('m'), $birthday->format('d'));
         if ($today->format('n') > $birthday->format('n') || ($today->format('n') == $birthday->format('n') && $today->format('J') > $birthday->format('j')))
         {
