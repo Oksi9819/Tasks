7e08cd712e0d00c43a2565fbf49da72a8a4dd5dd
OksiFoxy <oksanaostapuk@gmail.com>
2022-06-05 21:49:37 +0300

    Create Task10.php
    Test10

---
+++ Task10/Task10.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace src;
+
+
+use Exception;
+
+class Task10
+{
+    /**
+     * @throws Exception
+     */
+    public function main(int $input): void
+    {
+        if ($input>0) {
+            $result=array($input);
+            while($input > 1)
+            {
+                if ($input % 2 === 0) {
+                    $input = $input/2;
+                }else{
+                    $input = $input * 3 + 1;
+                }
+                array_push($result, $input);
+            }
+            print_r($result);
+        }else throw new Exception('Можно использовать только положительные целые числа.');
+    }
+}
